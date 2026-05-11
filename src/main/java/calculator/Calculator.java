becb417682563ebca1d4f202181ca331a23bf3b6
Louis Freier <Louisjamesfreier@gmail.com>
2026-05-11 07:06:52 +0200

    Anonyme Klasse durch Lamda ersetzt

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.*;
@@ -42,43 +43,42 @@ public class Calculator extends JFrame {
     private void setupOperationSelector() {
         operations = new HashMap<>();
         operations.put("Add", new Add());
-        operations.put("Sub", new Sub());
 
-        operations.put("Mul", new Operation(){
-            @Override
-                public int doOperation(int a, int b){
-                return a*b;
-            }
-        });
 
-        operations.put("Div", (int a, int b)->a/b);
 
         // TODO
         // Add a new operation "Sub" for the subtraction of two integers as an (instance of a) Java
         // class (you have yet to write this class)
+        operations.put("Sub", new Sub());
 
         // TODO
         // Add a new operation "Mul" for the multiplication of two integers as an anonymous class
+        operations.put("Mul", new Operation(){
+            @Override
+            public int doOperation(int a, int b){
+                return a*b;
+            }
+        });
 
         // TODO
         // Add a new operation "Div" for the division of two integers as a lambda expression
+        operations.put("Div", (int a, int b)->a/b);
+
 
         operationSelector = new JComboBox<>();
         operations.forEach((key, value) -> operationSelector.addItem(key));
 
         // TODO
         // Replace the anonymous class with a lambda expression
-        operationSelector.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        try {
-                            result.setText("" + calculate());
-                        } catch (NumberFormatException ex) {
-                            System.out.println("Invalid input.");
-                        }
-                    }
-                });
+
+        operationSelector.addActionListener((ActionEvent e) -> {
+            try {
+                result.setText("" + calculate());
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid input.");
+            }
+
+        });
     }
 
     /**
